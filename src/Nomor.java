6c9d8c39509affa1071abc18b2717d81581bfa23
acer <acer@192.168.56.1>
2018-11-04 12:43:20 +0700

    Membuat Class Nomor dan Constructor

---
+++ src/Nomor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class Nomor {
+    private String Nomor;
+    private String Operator;
+    
+    public Nomor(){
+        
+    }
+
+    public Nomor(String Nomor, String Operator) {
+        this.Nomor = Nomor;
+        this.Operator = Operator;
+    }
+    
+    
+}
