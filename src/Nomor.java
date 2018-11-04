d8bce062c03a0b3f183a27c35887be7595d442fd
acer <acer@192.168.56.1>
2018-11-04 12:45:38 +0700

    Mmebuat Getter dan Setter sekaligus output tampilan

--- src/Nomor.java
+++ src/Nomor.java
@@ -20,6 +20,27 @@ public class Nomor {
         this.Nomor = Nomor;
         this.Operator = Operator;
     }
+
+    public String getNomor() {
+        return Nomor;
+    }
+
+    public void setNomor(String Nomor) {
+        this.Nomor = Nomor;
+    }
+
+    public String getOperator() {
+        return Operator;
+    }
+
+    public void setOperator(String Operator) {
+        this.Operator = Operator;
+    }
     
-    
+    public String Info(){
+        String info = "";
+        System.out.println("Nomor   :"+this.Nomor+"\n");
+        System.out.println("Nomor   :"+this.Operator+"\n");
+        return info;
+    }
 }
